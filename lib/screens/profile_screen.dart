5f28a3ba0e773b3f31bf24f17869eb475cff2df0
Nida Damia <nidamianizam@gmail.com>
2022-05-12 12:18:28 +0800

    user profile

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,104 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:wefiwebu_2/screens/signin_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:wefiwebu_2/model/user_model.dart';
+
+
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreen createState() => _ProfileScreen();
+}
+
+class _ProfileScreen extends State<ProfileScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      this.loggedInUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        title: Text("Profile"),
+        centerTitle: true,
+      ),
+    body: Center(
+      child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 30,
+                ),
+                
+                SizedBox(height: 45,
+                  child: Text("Fullname: ${loggedInUser.fullname}",
+                    style: const TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w500
+                  ),
+                )
+                ),
+
+                SizedBox(height: 45,
+                child:Text("Email Address: ${loggedInUser.email}",
+                    style: const TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w500,
+                    )
+                  ),
+                ),
+
+                SizedBox(height: 45,
+                child:Text("Matric Number: ${loggedInUser.matric}",
+                    style: const TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w500,
+                    )
+                  ),
+                ),
+                
+                SizedBox(height: 45,
+                child:Text("Phone Number:${loggedInUser.mobilenum}",
+                    style: const TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w500,
+                    )
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 15,
+                ),
+                ActionChip(
+                    label: Text("Logout"),
+                    onPressed: () {
+                      logout(context);
+                }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => SigInScreen()));
+  }
+}
