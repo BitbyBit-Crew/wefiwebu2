caa6025a1b73b9a19b4a2a0ee410677356df6181
aimanarriady <87179900+aimanarriady@users.noreply.github.com>
2022-05-26 11:20:31 +0800

    markpetlace
    add button

--- lib/screens/Marketplace_screen.dart
+++ lib/screens/Marketplace_screen.dart
@@ -1,17 +1,105 @@
 import 'package:flutter/material.dart';
 
-class MarketplaceScreen extends StatefulWidget {
-  @override
-  State<MarketplaceScreen> createState() => _MarketplaceScreenState();
-}
+class MarketplaceScreen extends StatelessWidget {
+  Widget _buildFeaturedProduct(String name, double price, String image) {
+    return Card(
+      child: Container(
+        height: 250,
+        width: 160,
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 190,
+              width: 160,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/$image"),
+                ),
+              ),
+            ),
+            Text(
+              "\$ $price",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                  color: Color(0xff9b96d6)),
+            ),
+            Text(
+              name,
+              style: TextStyle(fontSize: 17),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
-class _MarketplaceScreenState extends State<MarketplaceScreen> {
+  final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: new Text("MarketPlace Page"),
+      key: _key,
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text("MarketPlace"),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.pink,
+        leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              // _key.currentState.openDrawer();
+            }),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
+          IconButton(icon: Icon(Icons.send), onPressed: () {}),
+        ],
       ),
+      // body: Container(
+      //   height: double.infinity,
+      //   width: double.infinity,
+      //   margin: EdgeInsets.symmetric(horizontal: 20),
+      //   child: Column(
+      //     children: <Widget>[
+      //       Container(
+      //         height: 120,
+      //         width: double.infinity,
+      //         child: Column(
+      //           mainAxisAlignment: MainAxisAlignment.end,
+      //           children: <Widget>[
+      //             Row(
+      //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //               children: <Widget>[
+      //                 Text("Featured Items",
+      //                     style: TextStyle(
+      //                         fontSize: 17, fontWeight: FontWeight.bold)),
+      //                 Text("See All",
+      //                     style: TextStyle(
+      //                         fontSize: 17, fontWeight: FontWeight.bold)),
+      //               ],
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //       Row(
+      //           children: <Widget>[
+      //             _buildFeaturedProduct(
+      //                 image: "Computer.jpg",
+      //                 price: 30.0,
+      //                 name: "Computer Science Book"),
+      //             _buildFeaturedProduct(
+      //                 image: "Computer.jpg",
+      //                 price: 30.0,
+      //                 name: "Computer Science Book"),
+      //           ],
+      //           ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,8 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:wefiwebu_2/model/user_model.dart';
+import 'package:wefiwebu_2/screens/Marketplace_screen.dart';
+import 'package:wefiwebu_2/screens/Addlostitem_screen.dart';
+import 'package:wefiwebu_2/screens/Addlostitem_screen.dart';
 import 'package:wefiwebu_2/screens/profile_screen.dart';
+<<<<<<< Updated upstream
 import 'package:wefiwebu_2/screens/profileupdate_screen.dart';
+=======
+import 'package:flutter/rendering.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+>>>>>>> Stashed changes
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -34,27 +42,47 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              //image setting
-              Image.asset(
-                "assets/images/logo.jpg",
-                fit: BoxFit.contain,
-                width: 100,
-              ),
-              Container(
-                //Text
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Welcome ${loggedInUser.fullname}"),
-              )
-            ],
-          ),
-          backgroundColor: Colors.pinkAccent,
+        home: Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            //image setting
+            Image.asset(
+              "assets/images/logo.jpg",
+              fit: BoxFit.contain,
+              width: 100,
+            ),
+            Container(
+              //Text
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Welcome ${loggedInUser.fullname}"),
+            )
+          ],
         ),
+        backgroundColor: Colors.pinkAccent,
       ),
-    );
+      body: const Center(),
+      floatingActionButton:
+          SpeedDial(icon: Icons.add, backgroundColor: Colors.black, children: [
+        SpeedDialChild(
+          child: const Icon(Icons.approval),
+          label: 'Lost & Found',
+          backgroundColor: Colors.grey,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Addlostitem_screen()),
+            );
+          },
+        ),
+        SpeedDialChild(
+          child: const Icon(Icons.account_balance_wallet),
+          label: 'Sell',
+          backgroundColor: Colors.grey,
+          // onTap: () {TBD},
+        ),
+      ]),
+    ));
   }
 }
